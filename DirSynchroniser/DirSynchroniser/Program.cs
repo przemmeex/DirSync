12a6d7c843c4a3fa6319faaba41947baa9ad2fbf
pk <przemmex@gmail.com>
2025-08-01 07:20:05 +0200

    improve input validation

--- DirSynchroniser/DirSynchroniser/Program.cs
+++ DirSynchroniser/DirSynchroniser/Program.cs
@@ -13,16 +13,13 @@ namespace DirSynchroniser
 
             try
             {
-                if (args.Length < 4)
+                string? parameterError = null;
+                if (args.Length < 4) parameterError = "usage: DirSynchroniser sourcePath targetPath intervalInSeconds logFilePath";
+                if (!Directory.Exists(args[0]) || !Directory.Exists(args[1]) || !Directory.Exists(args[3])) parameterError = "entered path(s) is invalid";
+                if (!int.TryParse(args[2], out int intervalSeconds)) parameterError = "enter an integer for interval";
+                if (parameterError != null)
                 {
-                    Console.WriteLine("usage: DirSynchroniser sourcePath targetPath intervalInSeconds logFilePath");
-                    return;
-                }
-
-
-                if (!int.TryParse(args[2], out int intervalSeconds))
-                {
-                    Console.WriteLine("enter an integer for interval");
+                    Console.WriteLine(parameterError);
                     return;
                 }
 
