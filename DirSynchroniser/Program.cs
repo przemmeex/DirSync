6a0e38574a3d0ce9bcbd5f7c100687e7cb13f287
przemmeex <46487880+przemmeex@users.noreply.github.com>
2025-08-01 09:17:23 +0200

    Update Program.cs
    minor parameters  validation fix

--- DirSynchroniser/Program.cs
+++ DirSynchroniser/Program.cs
@@ -15,8 +15,8 @@ namespace DirSynchroniser
             {
                 string? parameterError = null;
                 if (args.Length < 4) parameterError = "usage: DirSynchroniser sourcePath targetPath intervalInSeconds logFilePath";
-                if (!Directory.Exists(args[0]) || !Directory.Exists(args[1]) || !Directory.Exists(args[3])) parameterError = "entered path(s) is invalid";
-                if (!int.TryParse(args[2], out int intervalSeconds)) parameterError = "enter an integer for interval";
+                else if (!Directory.Exists(args[0]) || !Directory.Exists(args[1]) || !Directory.Exists(args[3])) parameterError = "entered path(s) is invalid";
+                else if (!int.TryParse(args[2], out int intervalSeconds)) parameterError = "enter an integer for interval";
                 if (parameterError != null)
                 {
                     Console.WriteLine(parameterError);
